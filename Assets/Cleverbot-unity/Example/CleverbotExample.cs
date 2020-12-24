d0d017e7001e86396d64bba7d9de4a793df6561d
Damien Mayance <apps@mayance.io>
2020-12-24 14:46:25 +0100

    :lipstick: Better UI

--- Assets/Cleverbot-unity/Example/CleverbotExample.cs
+++ Assets/Cleverbot-unity/Example/CleverbotExample.cs
@@ -5,61 +5,62 @@ using UnityEngine.UI;
 
 namespace CleverbotUnity
 {
-    public class CleverbotExample : MonoBehaviour
+  public class CleverbotExample : MonoBehaviour
+  {
+    [Header("Bindings")]
+    public InputField apiKeyField;
+    public InputField inputField;
+    public Text conversation;
+
+    private bool apiKeyChanged;
+    private Cleverbot clerverbot;
+
+    private void Awake()
     {
-        [Header("Bindings")] public InputField apiKeyField;
-        public InputField inputField;
-        public Text conversation;
+      apiKeyField.text = PlayerPrefs.GetString("Cleverbot_API_Key");
+      apiKeyField.onValueChanged.AddListener((text) =>
+      {
+        PlayerPrefs.SetString("Cleverbot_API_Key", text);
+        apiKeyChanged = true;
+        Debug.Log("New API Key");
+      });
 
-        private bool apiKeyChanged;
-        private Cleverbot clerverbot;
+      inputField.text = string.Empty;
+      inputField.onEndEdit.AddListener((t) => SendInput());
 
-        private void Awake()
-        {
-            apiKeyField.text = PlayerPrefs.GetString("Cleverbot_API_Key");
-            apiKeyField.onValueChanged.AddListener((text) =>
-            {
-                PlayerPrefs.SetString("Cleverbot_API_Key", text);
-                apiKeyChanged = true;
-                Debug.Log("New API Key");
-            });
-
-            inputField.text = string.Empty;
-            inputField.onEndEdit.AddListener((t) => SendInput());
-
-            conversation.text = string.Empty;
-        }
-
-        public async void SendInput()
-        {
-            var s = inputField.text;
-            inputField.text = string.Empty;
-            await SendInput(s);
-        }
-
-        public async Task SendInput(string text)
-        {
-            if (clerverbot == null || apiKeyChanged)
-            {
-                try
-                {
-                    clerverbot = new Cleverbot(apiKeyField.text);
-                }
-                catch (Exception e)
-                {
-                    conversation.text +=
-                        "\nAn error happened when establishing connection with the service.\n" + e.Message + "\n";
-                    Debug.LogException(e);
-                }
-            }
-
-            if (clerverbot == null) return;
-            
-            conversation.text += "> " + text + "\n";
-
-            var r = await clerverbot.GetResponseAsync(text);
-
-            conversation.text += "< " + r.Response + "\n";
-        }
+      conversation.text = "Welcome to the Cleverbot example. Type anything to start a conversation.\n\n";
     }
+
+    public async void SendInput()
+    {
+      var s = inputField.text;
+      inputField.text = string.Empty;
+      await SendInput(s);
+    }
+
+    public async Task SendInput(string text)
+    {
+      if (clerverbot == null || apiKeyChanged)
+      {
+        try
+        {
+          clerverbot = new Cleverbot(apiKeyField.text);
+        }
+        catch (Exception e)
+        {
+          conversation.text +=
+            "\nAn error happened when establishing connection with the service.\n" + e.Message + "\n";
+          Debug.LogException(e);
+        }
+      }
+
+      if (clerverbot == null) return;
+
+      conversation.text += "> " + text + "\n";
+
+      var r = await clerverbot.GetResponseAsync(text);
+
+      conversation.text += "< " + r.Response + "\n\n";
+    }
+  }
 }
